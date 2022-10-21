f6ff09e6a3c9f3245d8978b73eac83d87517375b
Georges Mbatou Charles DIEME <georgesmbatoucharles@gmail.com>
2022-10-21 00:30:24 +0000

    first Part Ok for Check

---
+++ index.py
@@ -0,0 +1,12 @@
+full_name = "Georges"
+message = "Welcome to Ahmed statements"
+print(full_name)
+print(message)
+
+for even_num in range(1, 31):
+    if even_num % 2 != 0:
+        print(even_num)
+
+for odd_num in range(1, 31):
+    if odd_num % 2 != 0:
+        print(odd_num)
